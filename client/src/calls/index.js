d6db036c9227989d6294830232628e0946e808a3
Yash020405 <yashaga0204@gmail.com>
2024-08-26 21:06:02 +0530

    Initial Commit

---
+++ client/src/calls/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+
+export const axiosInstance = axios.create({
+    headers : {
+        'Content-Type' : 'application/json',
+        'authorization' : `Bearer ${localStorage.getItem('token')}`
+    },
+    baseURL: 'http://localhost:9000'
+})
